9d804250af4011f53a8ea6a862173f997b489e92
Domenico Manna <mannadomenico2849@gmail.com>
2019-03-20 22:07:21 -0400

    Add user input

--- main.js
+++ main.js
@@ -1,12 +1,81 @@
-const numberOfGuesses = 3;
+const numberOfGuesses = 6;
 let remainingNumberOfGuesses = numberOfGuesses;
-const upperBound = 100;
-const winningNumber = generateRandomNumberBetween1And(upperBound);
+const playAgainMessage = 'Play again'
+const upperBound = 10;
+let winningNumber;
 
+let userInput = document.querySelector('.game__input');
+let btnAction = document.getElementById("action");
+let gameContainer = document.querySelector(".game");
+let gameHeading = document.querySelector(".game__heading");
+
+addClickListeners();
 setUpGame();
+
+function addClickListeners(){
+    btnAction.addEventListener('click', handleAction);
+    gameContainer.addEventListener('keypress', handleAction);
+    gameHeading.addEventListener('click', moveElement);
+}
+
+function moveElement(e){
+    e.target.classList.remove("move");
+    void e.target.offsetWidth;
+    e.target.classList.add("move");
+    
+}
+
 function setUpGame(){
+    userInput.disabled = false;
+    btnAction.textContent = 'Enter';
+    winningNumber = generateRandomNumberBetween1And(upperBound);
+    remainingNumberOfGuesses = numberOfGuesses;
     displayInstructions(upperBound);
-    displayNumberOfGuesses(numberOfGuesses);
+    displayFeedback(`You have ${numberOfGuesses} guesses`);
+}
+
+
+function handleAction(e) {
+    if (e.type !== 'click' && e.code !== 'Enter') return;
+    if (btnAction.textContent.includes(playAgainMessage)){
+        setUpGame();
+    }
+    else {
+        handleUserInput();
+    }
+}
+
+function handleUserInput() {
+    let userGuess = parseInt(userInput.value);
+    let gameFeedback;
+
+    if (userGuess === winningNumber){
+        gameFeedback = `You win! ${userGuess} was correct!`
+        endGame();
+    }
+
+    else {
+        remainingNumberOfGuesses --;
+        let guessesSingularOrPlural = getSingularOrPluralFormOfGuesses(remainingNumberOfGuesses);
+        gameFeedback = `${userGuess} is incorrect. You have ${remainingNumberOfGuesses} ${guessesSingularOrPlural} left`;
+    }
+
+    if (remainingNumberOfGuesses < 1){
+        gameFeedback = 'You lose';
+        endGame();
+    }
+    
+    userInput.value = '';
+    displayFeedback(gameFeedback);
+}
+
+function getSingularOrPluralFormOfGuesses(remainingNumberOfGuesses){
+    return (remainingNumberOfGuesses === 1) ? 'guess' : 'guesses';
+}
+
+function endGame(){
+    userInput.disabled = true;
+    btnAction.textContent = 'Play again';
 }
 
 function displayInstructions(upperBound){
@@ -14,9 +83,9 @@ function displayInstructions(upperBound){
     gameInstructions.textContent = `Guess a number between 1 and ${upperBound}`
 }
 
-function displayNumberOfGuesses(numberOfGuesses){
+function displayFeedback(message){
     let gameFeedback = document.querySelector('.game__feedback');
-    gameFeedback.textContent = `You have ${numberOfGuesses} guesses`
+    gameFeedback.textContent = message;
 }
 
 function generateRandomNumberBetween1And(upperBound){
